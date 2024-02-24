778a44b6a976af21d87f89b979d18b13a2ea2460
Camilla Devasini <camilladevasini@gmail.com>
2024-02-24 17:11:46 +0100

    test: add initial testing for useInfiniteScroll custom hook

---
+++ src/__test__/useInfiniteScroll.test.tsx
@@ -0,0 +1,62 @@
+import { renderHook } from "@testing-library/react";
+import { useInfiniteScroll } from "../utils/hooks/useInfiniteScroll";
+
+// Mock the fetch function
+global.fetch = jest.fn(
+  () =>
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          products: [
+            {
+              id: 0,
+              name: "",
+              imageUrl: "",
+              price: 0,
+              category: "",
+              itemInCart: 0,
+              loading: false,
+            },
+          ],
+        }),
+    }) as Promise<Response>
+);
+
+// Mock the IntersectionObserver
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  unobserve() {
+    return null;
+  }
+
+  root: Element | null = null;
+  rootMargin: string = "";
+  thresholds: number[] = [];
+  takeRecords(): IntersectionObserverEntry[] {
+    return [];
+  }
+};
+
+describe("useInfiniteScroll", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should initially loading, have no products and return no error", () => {
+    const loadingMoreRef = { current: document.createElement("div") };
+    const { result } = renderHook(() => useInfiniteScroll(loadingMoreRef));
+
+    expect(result.current.products).toEqual([]);
+    expect(result.current.isLoading).toBe(true);
+    expect(result.current.error).toBe(false);
+  });
+});
